deefa9452fda2d93a1b5e5d82f393e96339eb122
ovnemet <tavinemet@gmail.com>
2020-08-24 11:26:49 +0200

    third exercise added

---
+++ chapter_2/using_cout_3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using std::cout;
+
+int main() {
+	cout << "Degrees    Radians\n";
+	cout << "0          0.0000\n";
+	cout << "90         1.5708\n";
+	cout << "180        3.1416\n";
+	cout << "270        4.7124\n";
+	cout << "360        6.2832\n";
+}
