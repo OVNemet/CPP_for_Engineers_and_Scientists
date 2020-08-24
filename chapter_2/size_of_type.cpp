d22774a0ee438062b490ad507424c7dd3125a871
ovnemet <tavinemet@gmail.com>
2020-08-24 18:20:29 +0200

    size exercise added

---
+++ chapter_2/size_of_type.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout;
+
+int main() {
+	cout << "\nData Type  Bytes"
+		 << "\n---------  -----"
+		 << "\nint            " << sizeof(int)
+		 << "\nchar           " << sizeof(char)
+		 << "\nbool           " << sizeof(bool)
+		 << '\n';
+		 
+	return 0;
+}
